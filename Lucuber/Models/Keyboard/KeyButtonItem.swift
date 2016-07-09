6356ba94e81aba0823643e7974c71d4e5aadec4e
Hoooward <huoyunlong.ll@gmail.com>
2016-07-09 16:24:30 +0800

    分离代码，重置自定义键盘文件结构

---
+++ Lucuber/Models/Keyboard/KeyButtonItem.swift
@@ -0,0 +1,77 @@
+//
+//  KeyButtonItem.swift
+//  Lucuber
+//
+//  Created by Howard on 16/7/9.
+//  Copyright © 2016年 Howard. All rights reserved.
+//
+
+import Foundation
+
+class KeyButtonItem {
+    
+    enum KeyButtonType: String {
+        case Default = "Default"
+        case Bracket = "Bracket"
+        case Delete = "Delete"
+        case Shift = "Shift"
+        case Space = "Space"
+        case Return = "Return"
+    }
+    
+    enum TitleStatus {
+        case Lower
+        case Captal
+    }
+    
+    ///显示在按钮上的Title
+    var showTitle: String {
+        switch status {
+        case .Lower:
+            return lower
+        case .Captal:
+            return capital
+        }
+    }
+    ///大写字母
+    var capital = ""
+    ///小写字母
+    var lower = ""
+    ///按钮的类型
+    var type: KeyButtonType = .Default
+    ///标记大小写
+    var status: TitleStatus = .Captal
+    
+    init(dict: [String: AnyObject]) {
+        let type = dict["type"] as! String
+        let lower = dict["lower"] as! String
+        let capital = dict["capital"] as! String
+        
+        self.type = KeyButtonType(rawValue: type)!
+        self.lower = lower
+        self.capital = capital
+    }
+}
+
+class KeyButtonItemPackage: NSObject {
+    var topKeyboardItems = [KeyButtonItem]()
+    var bottomKeyboardItems = [KeyButtonItem]()
+    
+    class func loadPackage() -> KeyButtonItemPackage {
+        let path = NSBundle.mainBundle().pathForResource("FormulaKeyboard", ofType: ".plist")!
+        let dict = NSDictionary(contentsOfFile: path)!
+        
+        let keyButtonItemPackage = KeyButtonItemPackage()
+        if let items = dict["TopKeyboard"] as? [[String: AnyObject]] {
+            for item in items {
+                keyButtonItemPackage.topKeyboardItems.append(KeyButtonItem(dict: item))
+            }
+        }
+        if let items = dict["BottomKeyboard"] as? [[String: AnyObject]] {
+            for item in items {
+                keyButtonItemPackage.bottomKeyboardItems.append(KeyButtonItem(dict: item))
+            }
+        }
+        return keyButtonItemPackage
+    }
+}
---
+++ Lucuber/Views/AddFormula/Keyboard/KeyButton.swift
@@ -0,0 +1,61 @@
+//
+//  KeyButton.swift
+//  Lucuber
+//
+//  Created by Howard on 16/7/9.
+//  Copyright © 2016年 Howard. All rights reserved.
+//
+
+import Foundation
+class KeyButton: UIButton {
+    var item: KeyButtonItem? {
+        didSet {
+            if let item = item {
+                switch item.type {
+                case .Default:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardDefault"), forState: .Normal)
+                    setTitle(item.showTitle, forState: .Normal)
+                case .Bracket:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardBrackets"), forState: .Normal)
+                    setTitle(item.showTitle, forState: .Normal)
+                    setTitleColor(UIColor.whiteColor(), forState: .Normal)
+                case .Delete:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardDelete"), forState: .Normal)
+                case .Shift:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardShift"), forState: .Selected)
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardShiftSeleted"), forState: .Normal)
+                case .Space:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardSpace"), forState: .Normal)
+                case .Return:
+                    setBackgroundImage(UIImage(named: "FormulaKeyboardReturn"), forState: .Normal)
+                }
+            }
+        }
+    }
+    
+    //切换大小写
+    func changeKeyButtonTitle() {
+        if let item = item {
+            //修改按钮的大小写状态
+            item.status = item.status == .Captal ? .Lower : .Captal
+            switch item.type {
+            case .Default:
+                setTitle(item.showTitle, forState: .Normal)
+            default:
+                break
+            }
+        }
+    }
+    
+    init() {
+        super.init(frame: CGRectZero)
+        setTitleColor(UIColor(red: 160/255.0, green: 160/255.0, blue: 160/255.0, alpha: 1), forState: .Normal)
+        setTitleColor(UIColor.whiteColor(), forState: .Highlighted)
+        titleLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
