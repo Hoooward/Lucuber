bbb1fc45206782ef5ddf301b78d1683bf885c0f2
Hoooward <huoyunlong.ll@gmail.com>
2016-07-10 18:32:19 +0800

    创建New公式界面自定义sectionHearder和NameCell

---
+++ Lucuber/Views/AddFormula/NameTextViewCell/NameTextViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NameTextViewCell.swift
+//  Lucuber
+//
+//  Created by Howard on 7/10/16.
+//  Copyright © 2016 Howard. All rights reserved.
+//
+
+import UIKit
+
+class NameTextViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
