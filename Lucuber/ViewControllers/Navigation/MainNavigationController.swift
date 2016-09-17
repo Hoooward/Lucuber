923a0d0b3a442d3953b2d77ae8f33f09f9239b12
Hoooward <huoyunlong.ll@gmail.com>
2016-09-17 23:20:09 +0800

    分离 Storyboard

--- Lucuber/ViewControllers/Navigation/MainNavigationController.swift
+++ Lucuber/ViewControllers/Navigation/MainNavigationController.swift
@@ -1,5 +1,6 @@
 //
 //  MainNavigationController.swift
+
 //  Lucuber
 //
 //  Created by Tychooo on 16/9/17.
@@ -15,21 +16,5 @@ class MainNavigationController: UINavigationController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
