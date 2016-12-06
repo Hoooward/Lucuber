d4bfeb7b853e9bc2e8bdf76c9f4c617f191d9ce9
Hoooward <huoyunlong.ll@gmail.com>
2016-12-06 19:32:59 +0800

    添加 FeedURLCell, 尚未进行布局的 frame 计算

---
+++ Lucuber/Views/Cell/FeedCell/FeedURLCell.swift
@@ -0,0 +1,58 @@
+//
+//  FeedURLCell.swift
+//  Lucuber
+//
+//  Created by Tychooo on 16/12/6.
+//  Copyright © 2016年 Tychooo. All rights reserved.
+//
+
+import UIKit
+
+class FeedURLCell: FeedBaseCell {
+    
+    override class func heightOfFeed(feed: DiscoverFeed) -> CGFloat {
+        
+        let height = super.heightOfFeed(feed: feed) + (100 + 15)
+        
+        return ceil(height)
+    }
+    
+    public var tapURLInfoAction: ((URL) -> Void)?
+    
+    lazy var feedURLContainerView: FeedURLContainerView = {
+        let rect = CGRect(x: 0, y: 0, width: 200, height: 150)
+        let view = FeedURLContainerView(frame: rect)
+        view.compressionMode = false
+        return view
+    }()
+    
+    override func configureWithFeed(_ feed: DiscoverFeed, layout: FeedCellLayout, needshowCategory: Bool) {
+        
+        super.configureWithFeed(feed, layout: layout, needshowCategory: needshowCategory)
+        
+        if let attachment = feed.attachment {
+            
+            switch attachment {
+            case .URL(let openGraphInfo):
+                
+                feedURLContainerView.configureWithOpenGraphInfoType(openGraphInfo: openGraphInfo)
+                
+            default:
+                break
+            }
+        }
+        
+        // TODO: 没有进行 Layout 布局设置
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(feedURLContainerView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
