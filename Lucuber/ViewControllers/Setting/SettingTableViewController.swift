ffad0cfcecd0449b162670d61a99a84e579f0b36
Hoooward <huoyunlong.ll@gmail.com>
2016-12-13 03:40:06 +0800

    格式化部分代码

--- Lucuber/ViewControllers/Setting/SettingTableViewController.swift
+++ Lucuber/ViewControllers/Setting/SettingTableViewController.swift
@@ -13,53 +13,33 @@ class SettingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        let view = UIView()
     }
 
-  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
         tableView.deselectRow(at: indexPath, animated: true)
-        
+
         // 恢复备份
         if indexPath.row == 0 {
-            
+
             fetchDiscoverFormula(with: UploadFormulaMode.my , categoty: nil,  failureHandler: {
                 reason, errorMessage in
-                
+
                 defaultFailureHandler(reason, errorMessage)
-                
-//                self.isUploadingFormula = false
-                
-                
+
+                //                self.isUploadingFormula = false
+
+
             }, completion: { formulas in
-//                
                 printLog("更新数据成功")
-//                if self.formulasData.isEmpty {
-//                    
-//                } else {
-//                    
-//                    self.vistorView.removeFromSuperview()
-//                    self.searchBar.isHidden = false
-//                    self.isUploadingFormula = false
-//                    
-//                    self.collectionView?.reloadData()
-//                }
-                
-                
             })
-            
+
         }
-        
+
         // 上传备份
         if indexPath.row == 1 {
-            
-            
-//            pushCurrentUserUpdateInformation()
+            //            pushCurrentUserUpdateInformation()
         }
     }
 }
