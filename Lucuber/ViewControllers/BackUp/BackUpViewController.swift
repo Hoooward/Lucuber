bb26104213fc025b3a1b20e9c2351e2afc26dcac
Hoooward <huoyunlong.ll@gmail.com>
2017-01-23 22:30:47 +0800

    配置 LaunchScreen

--- Lucuber/ViewControllers/BackUp/BackUpViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BackUpViewController.swift
-//  Lucuber
-//
-//  Created by Tychooo on 17/1/22.
-//  Copyright © 2017年 Tychooo. All rights reserved.
-//
-
-import Foundation
---
+++ Lucuber/ViewControllers/Setting/NewScoreGroup/NewScoreGroupViewController.swift
@@ -0,0 +1,58 @@
+//
+//  NewScoreGroupViewController.swift
+//  Lucuber
+//
+//  Created by Tychooo on 16/12/26.
+//  Copyright © 2016年 Tychooo. All rights reserved.
+//
+
+import UIKit
+
+class NewScoreGroupViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    public var afterSelectedNewCategory: ((String) -> Void)?
+
+    fileprivate lazy var categorys: [String] = {
+        return self.loadCategoryFormMainBundle()
+    }()
+    
+    private func loadCategoryFormMainBundle() -> [String] {
+        let path = Bundle.main.path(forResource: "CubeCategory.plist", ofType: nil)!
+        let dict = NSDictionary(contentsOfFile: path)!
+        let categorysInChinese = dict["Chinese"] as! [String]
+        return categorysInChinese
+    }
+}
+
+extension NewScoreGroupViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categorys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CategoryCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.categoryLabel.text = categorys[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        defer {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        afterSelectedNewCategory?(categorys[indexPath.row])
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+class CategoryCell: UITableViewCell {
+    @IBOutlet weak var categoryLabel: UILabel!
+}
